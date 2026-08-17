cf775edd30e966950dd7b001e40353284ceb196b
wl <1983531544@qq.com>
2026-08-17 19:05:25 +0800

    refactor(ajax-package): 简化导出，去除默认导出
    - 移除 BaseApi 和 BaseHttpClient 的默认导出
    - 仅保留命名导出，减少包暴露接口复杂度
    - 保持其他导出 unchanged，包括错误处理和工厂方法

--- packages/ajax-package/src/index.ts
+++ packages/ajax-package/src/index.ts
@@ -1,5 +1,5 @@
-export { BaseApi, default as BaseApiDefault } from './BaseApi'
-export { BaseHttpClient, default as BaseHttpClientDefault } from './BaseHttpClient'
+export { BaseApi } from './BaseApi'
+export { BaseHttpClient } from './BaseHttpClient'
 export { HttpResponseError } from './errors'
 export { createHttpService, getHttpService } from './factory'
 export { getHttpService as default } from './factory'
